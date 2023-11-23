0ece53605962e90a335ab49eb39c9099dfbf978c
User <vladyslavd@yodagu.com>
2023-11-23 17:55:27 +0200

    1

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import RegForm from "./components/RegForm.vue";
 
 Vue.config.productionTip = false;
 
+const regForm = {
+  install: function (Vue, options) {
+    Vue.component("reg-form", RegForm);
+  },
+};
+
+export default regForm;
+
 new Vue({
   render: (h) =>
     h(RegForm, {
